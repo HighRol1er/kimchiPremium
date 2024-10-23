cc80b70412abf4b9100fee53e7789c74aac56dc6
HunterJoe <hunterjoe9999@gmail.com>
2024-10-23 15:23:11 +0900

    TableItem : 모든 데이터 포멧팅 완료

--- frontend/src/components/Homepage/TableComponents/TableItem.jsx
+++ frontend/src/components/Homepage/TableComponents/TableItem.jsx
@@ -2,17 +2,10 @@ import { useEffect, useState } from 'react';
 import { Tr, Td } from '@chakra-ui/react';
 import { FaStar } from 'react-icons/fa';
 import { getPriceDataFromBinance } from '../../../api/getExchangeData';
-// import { saveCoinsToWatchlist } from '../../functions/saveCoinsToWatchList';
-// import { removeCoinsToWatchlist } from '../../functions/removeCoinsToWatchlist';
 
 const TableItem = ({ coinData, usdKrw }) => {
   const [binanceCoinPrice, setBinanceCoinPrice] = useState();
-  const [convertedPrice, setConvertedPrice] = useState('');
-  console.log(usdKrw);
-  
-  // const watchlist =JSON.parse(localStorage.getItem("watchlist"));
-  
-  // const [isCoinInWatchlist, setIsCoinInWatchlist] = useState(watchlist?.includes(ticker));
+  const [binanceDollarToWon, setBinanceDollarToWon] = useState('');
   
   const ticker = coinData.market.replace('KRW-','');
   const upbitPrice = coinData.trade_price.toLocaleString();
@@ -20,40 +13,33 @@ const TableItem = ({ coinData, usdKrw }) => {
   const volume = (Number(coinData.acc_trade_price_24h) / 100_000_000).toFixed(0);
   const formatVolume = volume.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
 
-  // const checkWatchlist = (e) => {
-  //   if(watchlist) {
-  //     setIsCoinInWatchlist(prev => !prev);
-  //   } else {
-  //     setIsCoinInWatchlist(prev => !prev);
-  //   }
-  //   if(isCoinInWatchlist) {
-  //     removeCoinsToWatchlist(e, ticker, setIsCoinInWatchlist);
-  //   } else {
-  //     setIsCoinInWatchlist(true);
-  //     saveCoinsToWatchlist(e, ticker);
-  //   }
-  // };
   useEffect(() => {
     const getExchangeData = async () => {
       const coinPrice = await getPriceDataFromBinance(ticker);
       const formatCoinPrice = Number(coinPrice).toFixed(2);
+
       setBinanceCoinPrice(formatCoinPrice);
+
     };
 
     getExchangeData();
   },[]);
 
   useEffect(() => {
-    if (usdKrw && binanceCoinPrice) { // 둘 다 유효할 때만 계산
-      const usdKrwNumber = Number(usdKrw.replace(/,/g, ''));
-      const binancePriceNumber = Number(binanceCoinPrice);
-      setConvertedPrice((usdKrwNumber * binancePriceNumber).toFixed(2)); // 계산 결과를 상태에 저장
+    if (usdKrw && binanceCoinPrice) { 
+      // usdKrw, binanceCoinPrice의 값이 문자열이라 숫자로 변환 후 곱함 
+      const usdKrwToNumber = Number(usdKrw.replace(/,/g, ''));
+      const binancePriceToNumber = Number(binanceCoinPrice);
+      
+      setBinanceDollarToWon((usdKrwToNumber * binancePriceToNumber).toFixed(2));
     } else {
-      setConvertedPrice(''); // 초기화
+      setBinanceDollarToWon(''); // 초기화
     }
   }, [usdKrw, binanceCoinPrice]); // usdKrw 또는 binanceCoinPrices가 변경될 때마다 실행
   
 
+  const upbitPriceMinusBinancePrice = (Number(upbitPrice.replace(/,/g, '')) - Number(binanceDollarToWon)).toFixed(2);
+  const premiumPercentage = ((upbitPriceMinusBinancePrice / Number(binanceDollarToWon))* 100).toFixed(2);
   return (
     <>
       <Tr>
@@ -64,11 +50,14 @@ const TableItem = ({ coinData, usdKrw }) => {
           </div>
         </Td>
         <Td>{isNaN(binanceCoinPrice) ? "" : binanceCoinPrice}</Td>
-        <Td>{convertedPrice}</Td>
+        <Td>{isNaN(binanceDollarToWon) ? "" : binanceDollarToWon}</Td>
         <Td>{upbitPrice}</Td>
         <Td isNumeric>{change}%</Td>
         <Td isNumeric>{formatVolume} 억</Td>
-        <Td isNumeric> wait</Td>
+        <Td isNumeric>
+          {isNaN(upbitPriceMinusBinancePrice) ? "" : upbitPriceMinusBinancePrice}
+          {isNaN(premiumPercentage) ? "" : ` (${premiumPercentage}%)`} 
+        </Td>
       </Tr>
     </>
   )
